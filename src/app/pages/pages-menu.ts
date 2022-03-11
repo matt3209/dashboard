8e3f21b64152516d458a95ecd5067dc0f3e2bcb2
matt3209 <matthew.j.heider@gmail.com>
2022-03-10 18:21:09 -0600

    fix please

---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,77 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'Dashboard',
+    icon: 'shopping-cart-outline',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'City of Ether',
+    group: true,
+  },
+  {
+    title: 'Story (Coming Soon)',
+    icon: 'layout-outline',
+    children: [
+      {
+        title: 'Act 1',
+        link: '/pages/layout/stepper',
+      },
+    ],
+  },
+  {
+    title: 'Quest Board',
+    icon: 'edit-2-outline',
+    children: [
+      {
+        title: 'Gathering Resources',
+        link: '/pages/forms/inputs',
+      },
+      {
+        title: 'Potion Making',
+        link: '/pages/forms/layouts',
+      },
+      {
+        title: 'Diplomacy',
+        link: '/pages/forms/buttons',
+      },
+      {
+        title: 'Military',
+        link: '/pages/forms/datepicker',
+      },
+      {
+        title: 'Scholarship',
+        link: '/pages/forms/datepicker',
+      },
+    ],
+  },
+  {
+    title: 'Battle for Etheen (Coming Soon)',
+    icon: 'edit-2-outline',
+  },
+  {
+    title: 'Faction Rankings',
+    icon: 'keypad-outline',
+    link: '/pages/ui-features',
+    children: [
+      {
+        title: 'Grid',
+        link: '/pages/ui-features/grid',
+      },
+      {
+        title: 'Icons',
+        link: '/pages/ui-features/icons',
+      },
+      {
+        title: 'Typography',
+        link: '/pages/ui-features/typography',
+      },
+      {
+        title: 'Animated Searches',
+        link: '/pages/ui-features/search-fields',
+      },
+    ],
+  },
+];
