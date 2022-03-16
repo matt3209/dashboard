8992e48c3ca6fac4d498121211d281de2596aad3
matt3209 <matthew.j.heider@gmail.com>
2022-03-15 23:07:56 -0500

    metamask show

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -33,12 +33,20 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     const ethereumButton = document.querySelector('.enableEthereumButton');
-
+    const showAccount = document.querySelector('.showAccount');
+    
     ethereumButton.addEventListener('click', () => {
-      //Will Start the metamask extension
-      window.ethereum.request({ method: 'eth_requestAccounts' });
+      getAccount();
     });
+    
+    async function getAccount() {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = accounts[0];
+      showAccount.innerHTML = account;
+    }
+  
   }
+
 
   ngOnDestroy() {
     this.destroy$.next();
